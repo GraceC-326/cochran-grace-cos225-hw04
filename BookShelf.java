572cf3f417c3bf5b3c76ea31f2157a63cb33d5f4
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-23 18:52:16 -0500

    Updated BookShelf.java
    Added variables and default constructor, including the arrayList and the character.

--- BookShelf.java
+++ BookShelf.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+/*
+ Grace Cochran
+ COS 225: Homework 4
+ */
+
 public class BookShelf {
-    
+    public char firstLetter;
+    public ArrayList<Book> booksList;
+
+    public BookShelf(char shelfLetter){
+        firstLetter = shelfLetter;
+        booksList = new ArrayList<>();
+        for (int i=0; i < 8; i++){
+            booksList.add(null);
+        }
+    // Note: ask what dufour means by adding getters and setters here
+    }
+
 }

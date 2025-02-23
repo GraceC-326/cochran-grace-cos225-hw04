0d3b81c16b4202c5880b01ec2becdbbc94377e02
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-23 16:07:25 -0500

    Updated Book.java
    Gave Book.java its variables, constructor, and toString method needed for assignment.

--- Book.java
+++ Book.java
@@ -1,3 +1,14 @@
 public class Book {
-    
+    public String bookTitle;
+    public String bookGenre;
+
+    public Book(String title, String genre){
+        bookTitle = title;
+        bookGenre = genre;
+    }
+
+    @Override
+    public String toString(){
+    return bookTitle;
+    }
 }

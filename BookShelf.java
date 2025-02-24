4509ff16c8d3bcb480162b5d34d792be80a25fca
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-23 19:14:42 -0500

    Update BookShelf.java
    Added void method addBook, which adds a book, as per instruction.

--- BookShelf.java
+++ BookShelf.java
@@ -15,6 +15,23 @@ public class BookShelf {
             booksList.add(null);
         }
     // Note: ask what dufour means by adding getters and setters here
+    // im p sure this works
+    }
+
+    // Adds a book to a shelf if the book's title matches with the
+    // shelf's assigned letter. If it doesnt match, the book isn't added.
+    public void addBook(Book bookAdd){
+        // may have looked up help to find charAt exists hope that's cool 
+        char firstChar = bookAdd.bookTitle.charAt(0);
+        if (firstLetter == firstChar){
+            for (int i=0; i < booksList.size(); i++){
+                if (booksList.get(i) == null){
+                    booksList.set(i, bookAdd);
+                    return;
+                }
+            }  
+        }      
+        return;
     }
 
 }

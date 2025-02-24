655c52016c580c9b8beff44761a65c74d7f63c91
GraceC-326 <grace.a.cochran@maine.edu>
2025-02-23 19:51:17 -0500

    Updated LibraryTester.java
    Added main method and  all necessary lines of code, including instantiations, system prints, and addBook methods.

--- LibraryTester.java
+++ LibraryTester.java
@@ -1,3 +1,33 @@
 public class LibraryTester {
-    
+    public static void main(String[] args){
+
+        BookShelf oShelf = new BookShelf('O');
+        BookShelf tShelf = new BookShelf('T');
+        // reminder to self: chars are notified by ''
+        System.out.print(oShelf);
+        System.out.print(tShelf);
+
+        Book betrayed = new Book("The Heart of the Betrayed","Crime");
+        Book stars = new Book("Our Hill of Stars","Fantasy");
+        Book kind = new Book("One of a Kind","Science Fiction");
+        Book roses = new Book("The Vision of Roses","Romance");
+
+        System.out.print(betrayed);
+        System.out.print(stars);
+        System.out.print(kind);
+        System.out.print(roses);
+
+        oShelf.addBook(betrayed);
+        oShelf.addBook(stars);
+        oShelf.addBook(kind);
+        oShelf.addBook(roses);
+
+        tShelf.addBook(betrayed);
+        tShelf.addBook(stars);
+        tShelf.addBook(kind);
+        tShelf.addBook(roses);
+
+        System.out.print(oShelf);
+        System.out.print(tShelf);
+    }
 }
